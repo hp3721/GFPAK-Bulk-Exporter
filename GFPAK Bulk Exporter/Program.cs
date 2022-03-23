688902635f30533794e8ccde532e3b21f5c82138
BuildTools <44118666+hp3721@users.noreply.github.com>
2022-03-23 18:28:59 -0500

    Initial Commit

---
+++ GFPAK Bulk Exporter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using pkNX.Containers;
+
+namespace GFPAK_Bulk_Exporter
+{
+    internal class Program
+    {
+        static readonly ContainerHandler ProgressTracker = new();
+
+        static void Main(string[] args)
+        {
+            const string defaultPath = @"D:\Dumps\01001F5010DFA000\romfs\bin\archive";
+            var path = args.Length > 0 ? args[0] : defaultPath;
+            string PathGFPAK = path.Replace("archive", "extracted_archive");
+            FileMitm.SetRedirect(PathGFPAK, PathGFPAK);
+            ExtractGFPAKToFolder(path, PathGFPAK);
+        }
+
+        public static void ExtractGFPAKToFolder(string path, string dest)
+        {
+            Console.WriteLine("Dumping GFPAK files...");
+            var files = Directory.EnumerateFiles(path, "*.gfpak", SearchOption.AllDirectories);
+            foreach (var f in files)
+            {
+                try
+                {
+                    var gfpak = new GFPack(f);
+                    var rpath = f.Replace(path, dest);
+                    var dir = Path.GetDirectoryName(rpath);
+                    if (dir == null)
+                        throw new Exception("Bad directory?");
+                    Directory.CreateDirectory(dir);
+                    Console.WriteLine($"New GFPAK with {gfpak.Count} files.");
+                    gfpak.Dump(rpath, ProgressTracker);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
